b94d8bc9cf8204178e53720471951b28b11686d9
Vinh <v.vinhlt6@vinid.net>
2020-10-26 21:33:28 +0700

    Add dummy classes

---
+++ DummyRIchieSDK/src/main/java/com/example/dummyrichiesdk/RichieAndroid.kt
@@ -0,0 +1,21 @@
+package com.example.dummyrichiesdk
+
+import android.util.Log
+
+internal class RichieAndroid private constructor(private val flutterGateway: FlutterToNativeInterface) :
+    NativeToFlutterInterface {
+    override fun onLogout() {
+        Log.d("WTF", "Logout event from FLUTTER")
+    }
+
+    companion object {
+        var instance: RichieAndroid? = null
+            private set
+
+        fun init(flutterGateway: FlutterToNativeInterface) {
+            instance =
+                RichieAndroid(flutterGateway)
+        }
+    }
+
+}
